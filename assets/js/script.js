3e6243fc7506066e3cc329ed27eca4981654cfa2
ZaffID <dimasfazajulianputra@gmail.com>
2024-09-03 11:05:53 +0700

    bunga1.html

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+document.getElementById('loginForm').addEventListener('submit', function(event){
+    event.preventDefault();
+
+    const correctUsername = 'dimas';
+    const correctPassword = 'dimas';
+
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    if(username === correctUsername && password === correctPassword){
+        alert('Login berhasil!');
+        window.location.href = 'index.html'
+    } else{
+        alert('Login gagal');
+    }
+});
